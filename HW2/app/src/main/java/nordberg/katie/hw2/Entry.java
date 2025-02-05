2309fbd9db849ca55e06890ebcb1c58456f7c674
Katie <57237449+Katie-N@users.noreply.github.com>
2025-02-05 17:02:32 -0600

    Working thread!

--- HW2/app/src/main/java/nordberg/katie/hw2/Entry.java
+++ HW2/app/src/main/java/nordberg/katie/hw2/Entry.java
@@ -5,21 +5,35 @@ import android.os.Bundle;
 import android.util.Log;
 import android.Manifest;
 
+import java.util.Arrays;
 
+
+// Use this class for the startup stuff.
+// The app will go to this class first due to the Activity I added in the Manifest.
 public class Entry extends Activity {
 
     @Override
     protected void onCreate(Bundle b) {
         super.onCreate(b);
 
+        // Make a list of the permissions we want to ask for
         String[] PermsList = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
-        Log.d("debug", "Request permissions");
+        Log.d("Katie", "Request permissions");
+        // We have to request these permissions in the app too because they are dangerous
         this.requestPermissions(PermsList, 7);
     }
 
+    // When we get a response from the user regarding dynamic permissions, this is called
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        Log.d("debug", "Permission result");
+        Log.d("Katie", Arrays.toString(grantResults));
+
+        // Make an instance of the TaskThread class I made that will handle the actual hard work
+        // It's important to not clog the main thread (the UI thread) with stuff that can be done in the background
+        // I pass the context to the thread only because the context is needed for some of the function calls.
+        TaskThread tt = new TaskThread(this);
+        tt.run();
+
     }
 
 }
--- HW2/app/src/main/java/nordberg/katie/hw2/TaskThread.java
+++ HW2/app/src/main/java/nordberg/katie/hw2/TaskThread.java
@@ -1,40 +1,42 @@
-//package nordberg.katie.hw2;
-//
-//import android.app.Activity;
-//import android.content.pm.PackageManager;
-//import android.util.Log;
-//
-//import androidx.annotation.NonNull;
-//import androidx.core.app.ActivityCompat;
-//
-//import com.google.android.gms.location.FusedLocationProviderClient;
-//import com.google.android.gms.location.LocationServices;
-//import com.google.android.gms.tasks.OnSuccessListener;
-//
-//public class TaskThread extends Activity {
-////    @Override
-////    public void run(){
-//
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//    }
-//
-//    FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-//
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-//            Log.d("debug", "Permission granted");
-////            fusedLocationClient.getLastLocation().addOnSuccessListener(new LocListener());
-//            fusedLocationClient.getLastLocation().addOnSuccessListener(
-//                new OnSuccessListener<Location>() {
-//                    @Override
-//                    public void onSuccess(Location location) {
-//                        Log.d("debug", "Success getting LocListener");
-//                    }
-//                }
-//            );
-//        }
-//    }
-//}
-////TaskThread tt = new TaskThread();
-////tt.run();
+package nordberg.katie.hw2;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import android.Manifest;
+
+public class TaskThread extends Thread {
+    private Context context;
+
+    // Constructor to accept Context
+    public TaskThread(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void run(){
+
+        FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+
+        // Check if we have been granted the needed permissions before proceeding
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Log.d("Katie", "Permission granted");
+            // Get the last location and pass it to the anonymous class
+            fusedLocationClient.getLastLocation().addOnSuccessListener(
+                new OnSuccessListener<>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        Log.d("Katie", "Success getting Thread");
+                    }
+                }
+            );
+        }
+    }
+}
